8434c2e42b8ef564bfe105b63bce8785e9605e6e
TaroMurakami <muziflagmentfure@gmail.com>
2019-09-15 14:49:39 +0900

    109

--- 110/D.cpp
+++ 110/D.cpp
@@ -6,24 +6,24 @@ int mod = 1000000000+7;
 long long fac[510000], finv[510000], inv[510000];
  
 void factTable() {
-    fac[0] = fac[1] = 1;
-    finv[0] = finv[1] = 1;
-    inv[1] = 1;
-    for(int i=2; i<510000; i++){
-        fac[i] = (fac[i-1]*i) % mod;
-        inv[i] = mod - inv[mod%i] * (mod/i) % mod;
-        finv[i] = finv[i-1] * inv[i] % mod;
-    }
+  fac[0] = fac[1] = 1;
+  finv[0] = finv[1] = 1;
+  inv[1] = 1;
+  for(int i=2; i<510000; i++){
+    fac[i] = (fac[i-1]*i) % mod;
+    inv[i] = mod - inv[mod%i] * (mod/i) % mod;
+    finv[i] = finv[i-1] * inv[i] % mod;
+  }
 }
  
 long long combi(int n, int k){
-    if(n<k) {
-      return 0;
-    }
-    if(n<0 || k<0) {
-      return 0;
-    }
-    return fac[n] * (finv[k] * finv[n-k] % mod) % mod;
+  if(n<k) {
+    return 0;
+  }
+  if(n<0 || k<0) {
+    return 0;
+  }
+  return fac[n] * (finv[k] * finv[n-k] % mod) % mod;
 }
  
 int main() {
